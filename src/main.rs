23599c9aef805fcb41af5ffe3ad6aef483f030fa
Jack Forrest <jack@jrforrest.net>
2017-01-24 00:58:02 -0500

    Adds ncurses UI for watching the solver step on a board

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 extern crate rand;
+extern crate cursive;
 
 mod pos;
 mod board;
@@ -7,11 +8,40 @@ mod solver;
 
 use solver::Solver;
 
-fn main() {
-    let mut solver = Solver::new();
+use cursive::Cursive;
+use cursive::views::{TextView, IdView};
 
-    for _ in 0..10 {
-        println!("{}", solver.get_board());
-        solver.step();
-    }
+use std::fmt;
+
+use std::cell::RefCell;
+
+fn main() {
+    let mut ui = Cursive::new();
+
+    ui.add_global_callback('q', |u| u.quit());
+
+    let cell = RefCell::new(Solver::new());
+
+    let tv = TextView::new("Hello!");
+    let trv = IdView::new("text", tv);
+    ui.add_layer(trv);
+
+    ui.add_global_callback('s', move |u| {
+        let id = "text";
+        let view = u.find_id::<TextView>(id);
+
+        cell.borrow_mut().step();
+
+        match view {
+            None => panic!("Nothing found with id: {}", id),
+            Some(v) => v.set_content(format!("{}", cell.borrow().get_board())),
+        };
+    });
+
+    ui.run();
+
+   //# for _ in 0..10 {
+   //#     println!("{}", solver.get_board());
+   //#     solver.step();
+   //# }
 }
